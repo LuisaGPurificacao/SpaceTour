a7d75a5203798b728919a55d927f6c46e95ff970
LuisaPurifica <rm95324@fiap.com.br>
2022-06-09 19:43:37 -0300

    Finalização HTML e CSS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 
-/*const botaoinserir = document.getElementById("botaoinserir")
+const botaoinserir = document.getElementById("botaoinserir")
 const nome = document.getElementById("nome")
-const sobrenome = doc
+const sobrenome = document.getElementById("sobrenome")
+const CPF = document.getElementById("cpf")
+const data = document.getElementById("data")
+data.value= ''
+const motivacao =document.getElementById("motivacao")
 botaoinserir.addEventListener("click", verificar)
 function verificar(){
     if (nome.value ==''){
-        alert('Este campo está vazio')
+        alert('O campo nome está vazio')
     }
-    else if
+    else if(sobrenome.value ==''){
+        alert('O campo sobrenome está vazio')
+    }
+    else if(CPF.value ==''){
+        alert('O campo CPF está vazio')
+    }
+    else if( data.value == ''){
+        alert('oi')
+    }
+    else if(motivacao.value ==''){
+        alert('O campo motivação está vazio')
+    }
+    else{
+        alert('dados colocados inseridos, obrigado')
+    }
 }
-function xingarluisa(){
-    alert('vai se fuder luisa')
-}*/
 /*const botaoinserir = document.getElementById("botaoinserir")
 
 botaoinserir.addEventListener("click", function(){
     const campos = document.querySelectorAll("input['text'], input['date']")
     for(let x=0 ; x< campos.length ; x++){
         if (campos[x].value == ''){
-            alert('vai se fuder luisa')
         }
     }
 })*/

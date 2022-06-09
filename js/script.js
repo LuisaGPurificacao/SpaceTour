8503c40264d84a35bce619674880af946f62d0f9
LuisaPurifica <rm95324@fiap.com.br>
2022-06-09 19:30:47 -0300

    Página contato

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+/*const botaoinserir = document.getElementById("botaoinserir")
+const nome = document.getElementById("nome")
+const sobrenome = doc
+botaoinserir.addEventListener("click", verificar)
+function verificar(){
+    if (nome.value ==''){
+        alert('Este campo está vazio')
+    }
+    else if
+}
+function xingarluisa(){
+    alert('vai se fuder luisa')
+}*/
+/*const botaoinserir = document.getElementById("botaoinserir")
+
+botaoinserir.addEventListener("click", function(){
+    const campos = document.querySelectorAll("input['text'], input['date']")
+    for(let x=0 ; x< campos.length ; x++){
+        if (campos[x].value == ''){
+            alert('vai se fuder luisa')
+        }
+    }
+})*/
